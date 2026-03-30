991c194d365c7befede033fb2d1980f167d22412
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-30 17:09:06 -0300

    Herança: -Superclasse e Subclasse -Super no construtor da subclasse -Instaciação do objeto

---
+++ src/Program.java
@@ -0,0 +1,21 @@
+import entities.Account;
+import entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+        BusinessAccount account2 = new BusinessAccount(1,"Victor",1500.00,2500.00);
+        Account account3 = new Account();
+        Account account4 = new Account(1, "Juliana", 1500.00);
+
+
+
+
+
+
+
+
+
+    }
+}
